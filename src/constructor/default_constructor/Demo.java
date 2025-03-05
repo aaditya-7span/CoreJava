582c61a9c8132e9d7ae7100d9753bdc5d73ffbe6
AadityaJatale <aadityajatale5@gmail.com>
2025-03-05 15:28:38 +0530

    constructor practicals performed by with all the three types of constructor copy, parameterized and default constructor with its proper example.

---
+++ src/constructor/default_constructor/Demo.java
@@ -0,0 +1,8 @@
+package constructor.default_constructor;
+
+public class Demo {
+    public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount();
+        acc1.display();
+    }
+}
