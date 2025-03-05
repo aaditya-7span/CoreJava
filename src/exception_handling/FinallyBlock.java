b5a46747d1872b1a72d614508599ea2b4df7a3ec
AadityaJatale <aadityajatale5@gmail.com>
2025-03-05 15:47:52 +0530

    excpetion handling practical performed with the proper and relevent example for each exception and handled according to it

---
+++ src/exception_handling/FinallyBlock.java
@@ -0,0 +1,14 @@
+package exception_handling;
+
+public class FinallyBlock {
+    //finally Block
+    public static void main(String[] args) {
+        try {
+            int num = 10 / 0;
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        } finally {
+            System.out.println("This block always executes.");
+        }
+    }
+}
