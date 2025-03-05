47af073910e313be0a549606d9f2db0f99ec7850
AadityaJatale <aadityajatale5@gmail.com>
2025-03-05 17:33:29 +0530

    performed overriding method with the example of the notificationservice and the bookstore example.

---
+++ src/overriding/BookStoreService.java
@@ -0,0 +1,20 @@
+package overriding;
+
+//Changing the Order of the Parameters of Methods
+public class BookStoreService {
+
+    public void purchaseBook(String title, String author, int quantity) {
+        System.out.println("Purchasing " + quantity + " copies of " + title + " by " + author + ".");
+    }
+
+    public void purchaseBook(int quantity, String title, String author) {
+        System.out.println("Purchasing " + quantity + " copies of " + title + " by " + author + ".");
+    }
+
+    public static void main(String[] args) {
+        BookStoreService bookstoreService = new BookStoreService();
+
+        bookstoreService.purchaseBook("rich dad poor dad", "ram lakhan", 3);
+        bookstoreService.purchaseBook(20, "maths text book", "mukeshbhai pathak");
+    }
+}
